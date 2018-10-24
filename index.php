3fd0d9f066024a05393855acfd8739819332d535
RobotDucksson <robert@anderssons.info>
2018-10-24 13:25:33 +0200

    css fix maybe

--- index.php
+++ index.php
@@ -6,15 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-
-
     <title>BlockedEV</title>
-
     <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="/css/bootstrap.min.css">
-
-    <!-- Custom styles for this template -->
-    <link href="signin.css" rel="stylesheet">
+    <link rel="stylesheet" href="css\bootstrap.min.css">
   </head>
 
   <body class="text-center">
